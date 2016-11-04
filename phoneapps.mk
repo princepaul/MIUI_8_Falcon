62bc6bee5a6100f27b7515ee44af98a59a67bc09
Kowak <shwkxz@gmail.com>
2016-11-04 14:18:28 -0200

    initial workspace

---
+++ phoneapps.mk
@@ -0,0 +1,41 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	BasicDreams \
+	Bluetooth \
+	BluetoothExt \
+	BluetoothMidiService \
+	CertInstaller \
+	com.qualcomm.location \
+	com.qualcomm.services.location \
+	Galaxy4 \
+	KeyChain \
+	LatinIME \
+	LiveWallpapers \
+	NoiseField \
+	PacProcessor \
+	PhaseBeam \
+	PhotoTable \
+	PicoTts \
+	PrintSpooler \
+	qcrilmsgtunnel \
+	Stk \
+	telresources \
+	TimeService \
+	WAPPushManager
+
+private-phone-priv-apps := \
+	BackupRestoreConfirmation \
+	CellBroadcastReceiver \
+	CMAudioService \
+	CMSettingsProvider \
+	ExternalStorageProvider \
+	FusedLocation \
+	InputDevices \
+	ProxyHandler \
+	Screencast \
+	SharedStorageBackup \
+	Shell \
+	ThemesProvider 
+	
